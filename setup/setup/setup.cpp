d1ab33c8df31693b952a3109436bff9d89ad5462
Kevin <1069440019@qq.com>
2018-10-06 21:52:43 +0800

    init program

---
+++ setup/setup/setup.cpp
@@ -0,0 +1,14 @@
+﻿// setup.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <windows.h>  
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+
+
