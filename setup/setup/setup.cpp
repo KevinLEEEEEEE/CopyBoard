6559e052171aa4a91439bc0c9f3930dcbac677e6
Kevin <1069440019@qq.com>
2018-10-07 18:08:40 +0800

    runable

--- setup/setup/setup.cpp
+++ setup/setup/setup.cpp
@@ -1,14 +1,29 @@
-﻿// setup.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿
 #include "pch.h"
 #include <windows.h>  
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
 
-#include <cstdlib>
-#include <cstdio>
-#include <cmath>
+int main(int argc, char** argv) {
+     glutInit(&argc, argv);
+     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+     glutInitWindowSize(350, 150);
+     glutCreateWindow(argv[0]);
+     
+	 glClearColor(0.0, 0.0, 0.0, 0.0);
+	 glClear(GL_COLOR_BUFFER_BIT);
+	 glColor3f(1.0, 1.0, 1.0);
+	 glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+	 glBegin(GL_POLYGON);
+	 glVertex3f(0.25, 0.25, 0.0);
+	 glVertex3f(0.75, 0.25, 0.0);
+	 glVertex3f(0.75, 0.75, 0.0);
+	 glVertex3f(0.25, 0.75, 0.0);
+	 glEnd();
+	 glFlush();
+	 glutMainLoop();
+     return 0;
+}
 
 
